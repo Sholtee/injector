e0aa158e0ef8594064d2318417b873aa4ca45702
Dénes Solti <sodnaatx@gmail.com>
2019-05-17 07:37:20 +0200

    InterfaceProxyTests

---
+++ SRC/Injector.Tests/InterfaceProxy/InterfaceProxyTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+
+namespace Solti.Utils.InterfaceProxy.Tests
+{
+    [TestFixture]
+    public sealed class InterfaceProxyTests
+    {
+        private interface IMyInterface
+        {
+            int Hooked(int val);
+            int NotHooked(int val);
+        }
+
+        private sealed class MyClass : IMyInterface
+        {
+            public int Hooked(int val)
+            {
+                return val;
+            }
+
+            public int NotHooked(int val)
+            {
+                return val;
+            }
+        }
+
+        private sealed class MyProxy : DI.InterfaceProxy<IMyInterface>
+        {
+            public MyProxy() : base(new MyClass())
+            {
+            }
+
+            protected override object Invoke(MethodInfo targetMethod, object[] args)
+            {
+                if (targetMethod.Name == nameof(Target.Hooked)) return 1986;
+                return base.Invoke(targetMethod, args);
+            }
+        }
+
+        [Test]
+        public void InterfaceProxy_ShouldHook()
+        {
+            IMyInterface myInterface = new MyProxy().Proxy;
+
+            Assert.That(myInterface.NotHooked(1), Is.EqualTo(1));
+            Assert.That(myInterface.Hooked(1), Is.EqualTo(1986));
+        }
+    }
+}
