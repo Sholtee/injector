0c7662d1b78647c35ee37ded96973220f4a06fcb
Dénes Solti <sodnaatx@gmail.com>
2019-05-21 06:48:59 +0200

    IComposite

---
+++ SRC/Private/Patterns/Composite.cs
@@ -0,0 +1,73 @@
+﻿/********************************************************************************
+* Composite.cs                                                                  *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solti.Utils.DI
+{
+    public abstract class Composite<T>: Disposable, IComposite<T> where T: Composite<T>
+    {
+        #region Private
+        private /*readonly*/ IDictionary<T, byte> FChildren = new ConcurrentDictionary<T, byte>();
+        private /*readonly*/ T FParent;
+        #endregion
+
+        #region Protected
+        protected Composite(T parent)
+        {
+            FParent = parent;
+        }
+
+        protected override void Dispose(bool disposeManaged)
+        {
+            if (disposeManaged)
+            {
+                //
+                // Kivesszuk magunkat a szulo gyerekei kozul (kiveve ha gyoker elemunk van,
+                // ott nincs szulo).
+                //
+
+                if (FParent != null)
+                {
+                    FParent.FChildren.Remove((T) this);
+                    FParent = null;
+                }
+
+                //
+                // Osszes gyereket eltavolitjuk majd toroljuk a listat.
+                //
+
+                foreach (IDisposable child in FChildren.Keys)
+                {
+                    child.Dispose();     
+                }
+
+                FChildren.Clear();
+                FChildren = null;
+            }
+
+            base.Dispose(disposeManaged);
+        }
+
+        protected abstract T CreateChild();
+        #endregion
+
+        #region IComposite
+        T IComposite<T>.Parent => FParent;
+
+        IReadOnlyList<T> IComposite<T>.Children => FChildren.Keys.ToArray();
+
+        T IComposite<T>.CreateChild()
+        {
+            T result = CreateChild();
+            FChildren.Add(result, 0);
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ SRC/Private/Patterns/IComposite.cs
@@ -0,0 +1,17 @@
+﻿/********************************************************************************
+* IComposite.cs                                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace Solti.Utils.DI
+{
+    public interface IComposite<T>: IDisposable where T : IComposite<T>
+    {
+        T Parent { get; }
+        IReadOnlyList<T> Children { get; }
+        T CreateChild();
+    }
+}
