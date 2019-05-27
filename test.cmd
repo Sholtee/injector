f274f64b9b0a5267e03858abc3f3e5ef1317542a
Dénes Solti <sodnaatx@gmail.com>
2019-05-27 07:01:07 +0200

    test.cmd II

--- test.cmd
+++ test.cmd
@@ -5,14 +5,4 @@
 ::
 @echo off
 
-set framework=netcoreapp2.2
-set bin=%~dp0BIN
-set testrunner=%bin%\testrunner
-
-call nuget install "NUnit.Console" -Framework %framework% -Version 3.10.0 -OutputDirectory "%testrunner%"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-call dotnet build Injector.sln --configuration Debug 
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-call dotnet %testrunner%\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe "%bin%\%framework%\Injector.Test.dll"
+dotnet test "%~dp0TEST\Injector.Tests.csproj" --configuration Debug
