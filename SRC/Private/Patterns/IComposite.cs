611b015712564b0514de479f8a1224a2977dcc0c
Dénes Solti <sodnaatx@gmail.com>
2019-05-30 17:27:12 +0200

    IComposite.cs dox

--- SRC/Private/Patterns/IComposite.cs
+++ SRC/Private/Patterns/IComposite.cs
@@ -8,6 +8,11 @@ using System.Collections.Generic;
 
 namespace Solti.Utils.DI.Internals
 {
+    /// <summary>
+    /// Describes the composite pattern.
+    /// </summary>
+    /// <typeparam name="T">The type on which we want to apply the composite pattern.</typeparam>
+    /// <remarks>This is an internal interface so it can be changed from version to version. Don't use it!</remarks>
     public interface IComposite<T>: IDisposable where T : IComposite<T>
     {
         /// <summary>
@@ -21,9 +26,19 @@ namespace Solti.Utils.DI.Internals
         ICollection<T> Children { get; }
 
         /// <summary>
-        /// Creates a new child entity and puts into the children list.
+        /// Creates a new child entity and puts it into the <see cref="Children"/> list.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The newly created child</returns>
+        /// <remarks>
+        /// <list type="bullet">
+        /// <item>
+        /// <description>The child created by this method should inherit the state of this entity (but modifying the child should not affect it).</description>
+        /// </item>
+        /// <item>
+        /// <description>Disposing the child should cause its removal from the <see cref="Children"/> list. Without it, this entity is responsible for freeing it on dispose.</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         T CreateChild();
 
         /// <summary>
