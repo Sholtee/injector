c93936031bbc88932275e063ddf05406e2d633b4
Dénes Solti <sodnaatx@gmail.com>
2019-05-30 17:54:54 +0200

    ServiceAlreadyRegisteredException.cs dox

--- SRC/Public/Exceptions/ServiceAlreadyREgisteredException.cs
+++ SRC/Public/Exceptions/ServiceAlreadyREgisteredException.cs
@@ -9,8 +9,16 @@ namespace Solti.Utils.DI
 {
     using Properties;
 
+    /// <summary>
+    /// The exception that is thrown when a service has already been registered.
+    /// </summary>
     public sealed class ServiceAlreadyRegisteredException: ArgumentException
     {
+        /// <summary>
+        /// Creates a new <see cref="ServiceAlreadyRegisteredException"/> instance.
+        /// </summary>
+        /// <param name="iface">The interface which already was registered.</param>
+        /// <param name="innerException">The inner exception (if present).</param>
         public ServiceAlreadyRegisteredException(Type iface, Exception innerException = null): base(string.Format(Resources.ALREADY_REGISTERED, iface), innerException)
         {
         }
