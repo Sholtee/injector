15d6349a9f61c24d95b30108f9ab92a71b6bb9d9
Dénes Solti <sodnaatx@gmail.com>
2019-05-30 16:41:50 +0200

    QueryServiceInfo() tests

---
+++ TEST/Injector/QueryServiceInfo.cs
@@ -0,0 +1,37 @@
+﻿/********************************************************************************
+* QueryServiceInfo.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.DI.Tests
+{
+    using Properties;
+
+    [TestFixture]
+    public sealed partial class InjectorTests
+    {
+        [Test]
+        public void QueryServiceInfo_ShouldThrowOnNonRegisteredService()
+        {
+            Assert.Throws<NotSupportedException>(() => Injector.QueryServiceInfo<IDisposable>(), string.Format(Resources.DEPENDENCY_NOT_FOUND, typeof(IDisposable)));
+        }
+
+        [Test]
+        public void QueryServiceInfo_ShouldWorkWithGenericTypes()
+        {
+            Injector.Service(typeof(IInterface_3<>), typeof(Implementation_3<>));
+
+            IServiceInfo info = Injector.QueryServiceInfo(typeof(IInterface_3<>));
+            Assert.That(info.IsService);
+
+            IServiceInfo anotherInfo = Injector.QueryServiceInfo<IInterface_3<string>>();
+            Assert.That(anotherInfo.IsService);
+
+            Assert.AreNotSame(info, anotherInfo);
+        }
+    }
+}
