9e32c6f9677c6027173030ad1f4352c006e22646
Dénes Solti <sodnaatx@gmail.com>
2019-06-07 07:29:44 +0200

    InjectorEntry fix [TypeResolver should be triggered only once, cloning should not trigger the resolver]

--- SRC/Private/InjectorEntry.cs
+++ SRC/Private/InjectorEntry.cs
@@ -10,40 +10,55 @@ namespace Solti.Utils.DI.Internals
 {
     using Properties;
 
-    internal sealed class InjectorEntry: Disposable, IServiceInfo, ICloneable
+    internal sealed class InjectorEntry: Disposable, IServiceInfo, ICloneable // TODO: !!TESTS!!
     {
         private Func<IInjector, Type, object> FFactory;
+        private Type FImplementation;
         private object FValue;
-        private readonly ITypeResolver FImplementation;
+        private readonly ITypeResolver FResolver;
 
-        private sealed class DefaultTypeResolver : ITypeResolver
+        //
+        // Kidelegalja a tipus lekerdezeset a szulo entitasnak. Ez a trukk arra van h ha klonozzuk a 
+        // bejegyzest a resolver akkor is csak egyszer keruljon meghivasra fuggetlenul attol melyik 
+        // bejegyzesben volt hivatkozva a tipus.
+        //
+
+        private sealed class DelegateTypeResolver : ITypeResolver
         {
-            private Type Interface { get; }
-            private Type Implementation { get; }
+            private InjectorEntry Parent { get; }
 
-            public DefaultTypeResolver(Type @interface, Type implementation)
-            {
-                Interface      = @interface;
-                Implementation = implementation;
-            }
+            public DelegateTypeResolver(InjectorEntry parent) => Parent = parent;
 
             Type ITypeResolver.Resolve(Type @interface)
             {
-                Debug.Assert(@interface == Interface);
-                return Implementation;
+                Debug.Assert(@interface == Parent.Interface);
+                return Parent.Implementation;
             }
         }
 
         #region Immutables (IServiceInfo)
         public Type Interface { get; }
 
-        public Type Implementation => FImplementation?.Resolve(Interface);
+        public Type Implementation
+        {
+            get
+            {
+                if (FImplementation == null)
+                {
+                    if (FResolver != null) lock (FResolver)
+                    {
+                        if (FImplementation == null) FImplementation = FResolver.Resolve(Interface);
+                    }
+                }
+                return FImplementation;
+            }
+        }
 
         public Lifetime? Lifetime { get; }
 
-        public bool IsService => FImplementation != null;
+        public bool IsService => FImplementation != null || FResolver != null;
 
-        public bool IsLazy => IsService && !(FImplementation is DefaultTypeResolver);
+        public bool IsLazy => FResolver != null;
 
         public bool IsFactory => !IsService && Factory != null;
 
@@ -80,19 +95,20 @@ namespace Solti.Utils.DI.Internals
         }
         #endregion
 
-        public InjectorEntry(Type @interface, Type implementation = null, Lifetime? lifetime = null): this
-        (
-            @interface, 
-            implementation != null ? new DefaultTypeResolver(@interface, implementation) : null, 
-            lifetime
-        ) {}
+        public InjectorEntry(Type @interface, Type implementation = null, Lifetime? lifetime = null)
+        {
+            Interface = @interface;
+            Lifetime = lifetime;
 
-        public InjectorEntry(Type @interface, ITypeResolver implementation, Lifetime? lifetime = null)
+            FImplementation = implementation;
+        }
+
+        public InjectorEntry(Type @interface, ITypeResolver resolver, Lifetime? lifetime = null)
         {
             Interface = @interface;
             Lifetime  = lifetime;
 
-            FImplementation = implementation;
+            FResolver = resolver;
         }
 
         public object Clone()
@@ -103,8 +119,8 @@ namespace Solti.Utils.DI.Internals
                 throw new InvalidOperationException(Resources.CANT_CLONE);
 
             //
-            // 1) Hogy klonozaskor ne legyen a TypeResolver triggerelve ezert magat a resolvert
-            //    adjuk at.
+            // 1) Hogy klonozaskor egyaltalan ne es kesobb is maximum csak egyszer legyen a TypeResolver 
+            //    triggerelve ezert sajat resolvert adunk at.
             //
             // 2) Ha a peldany regisztralasakor a "releaseOnDispose" igazra volt allitva akkor
             //    a peldany is lehet Singleton. Viszont mi nem akarjuk h a gyermek injektor
@@ -112,7 +128,7 @@ namespace Solti.Utils.DI.Internals
             //    nem masoljuk.
             //
 
-            return new InjectorEntry(Interface, FImplementation, IsInstance ? null : Lifetime)
+            return new InjectorEntry(Interface, new DelegateTypeResolver(this), IsInstance ? null : Lifetime)
             {
                 Factory = Factory,
 
