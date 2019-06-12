5a43ec79f3a585012991545bc043a28b68643c8a
Dénes Solti <sodnaatx@gmail.com>
2019-06-12 10:43:32 +0200

    pack.cmd: package version from csproj

--- pack.cmd
+++ pack.cmd
@@ -5,9 +5,17 @@
 ::
 @echo off
 
+set verFile=".\version"
+
+call powershell -nologo -noprofile -command "$xml = [Xml] (Get-Content .\SRC\Injector.csproj); $xml.Project.PropertyGroup.Version" > %verFile%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set /p ver=<%verFile%
+del %verFile%
+
 if exist "BIN" (
 	@echo cleanup...
-    call rmdir /Q /S "BIN"
+    call rmdir /Q /S ".\BIN"
 )
 
 setlocal enabledelayedexpansion
@@ -15,9 +23,9 @@ for /f %%i in (targets) do (
     @echo: 
     @echo build against: %%i
 
-	call dotnet build "SRC\Injector.csproj" -c Release -p:TargetFramework=%%i;NoDocfx=True
-	if !errorlevel! neq 0 exit /b !errorlevel!
+    call dotnet build "SRC\Injector.csproj" -c Release -p:TargetFramework=%%i;NoDocfx=True
+    if !errorlevel! neq 0 exit /b !errorlevel!
 )
 
-call nuget pack "Injector.nuspec" -OutputDirectory "BIN"
+call nuget pack "Injector.nuspec" -OutputDirectory "BIN" -Version %ver%
 
