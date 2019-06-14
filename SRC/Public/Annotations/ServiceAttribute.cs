fc97c31c9b87421e6e4a73c923a7fdbc137c08dc
Dénes Solti <sodnaatx@gmail.com>
2019-06-14 08:26:04 +0200

    ConfigFromAssembly

---
+++ SRC/Public/Annotations/ServiceAttribute.cs
@@ -0,0 +1,32 @@
+﻿/********************************************************************************
+* ServiceAttribute.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI
+{
+    /// <summary>
+    /// Indicates that a class is being used as a service.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class ServiceAttribute: Attribute
+    {
+        /// <summary>
+        /// The service interface implemented by the marked class.
+        /// </summary>
+        public Type Interface { get; }
+
+        /// <summary>
+        /// The lifetime of the service.
+        /// </summary>
+        public Lifetime Lifetime { get; }
+
+        public ServiceAttribute(Type @interface, Lifetime lifetime = Lifetime.Transient)
+        {
+            Interface = @interface;
+            Lifetime  = lifetime;
+        }
+    }
+}
---
+++ SRC/Public/Extensions/Config/ConfigFromAssembly.cs
@@ -0,0 +1,32 @@
+﻿/********************************************************************************
+* ConfigFromAssembly.cs                                                         *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Solti.Utils.DI.Internals
+{
+    /// <summary>
+    /// Provides an extension to setup the injector.
+    /// </summary>
+    public static class ConfigFromAssembly
+    {
+        /// <summary>
+        /// Registers the annotated services from the given assembly.
+        /// </summary>
+        /// <param name="target">The target injector.</param>
+        /// <param name="assembly">The source assembly.</param>
+        /// <returns>The injector itself.</returns>
+        public static IInjector Setup(this IInjector target, Assembly assembly)
+        {
+            foreach (Type service in assembly.DefinedTypes.Where(t => t.IsClass))
+                foreach (ServiceAttribute serviceAttribute in service.GetCustomAttributes<ServiceAttribute>())
+                    target.Service(serviceAttribute.Interface, service, serviceAttribute.Lifetime);
+
+            return target;
+        }
+    }
+}
