f9ab40539feb317fff17d38f6106ff81604a41d2
Dénes Solti <sodnaatx@gmail.com>
2019-06-25 12:54:27 +0200

    Cache.cs: scopes

--- SRC/Private/Cache.cs
+++ SRC/Private/Cache.cs
@@ -5,13 +5,14 @@
 ********************************************************************************/
 using System;
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 
 namespace Solti.Utils.DI.Internals
 {
     internal static class Cache<TKey, TValue>
     {
-        private static readonly ConcurrentDictionary<TKey, TValue> FCache = new ConcurrentDictionary<TKey, TValue>();
+        private static readonly ConcurrentDictionary<object, TValue> FCache = new ConcurrentDictionary<object, TValue>();
 
-        public static TValue GetOrAdd(TKey key, Func<TValue> factory) => FCache.GetOrAdd(key, @void => factory());
+        public static TValue GetOrAdd(TKey key, Func<TValue> factory, [CallerMemberName] string scope = "") => FCache.GetOrAdd(new {scope, key}, @void => factory());
     }
 }
