2997fd0f1fa65fcfeca78c089af4a9b885d07e45
Dénes Solti <sodnaatx@gmail.com>
2019-07-29 18:18:26 +0200

    Perf/Program: debug config

--- PERF/Program.cs
+++ PERF/Program.cs
@@ -1,9 +1,21 @@
-﻿using BenchmarkDotNet.Running;
+﻿/********************************************************************************
+* Program.cs                                                                    *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
 
 namespace Solti.Utils.DI.Perf
 {
     class Program
     {
-        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run
+        (
+            args
+#if DEBUG
+            , new DebugInProcessConfig()
+#endif
+        );
     }
 }
