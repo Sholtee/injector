d63adf4246340ea6be7adbaa9b83ef7e913f168a
Dénes Solti <sodnaatx@gmail.com>
2019-08-25 08:55:33 +0200

    ReaderWriterLockExtensions

---
+++ SRC/Private/Extensions/ReaderWriterLockExtensions.cs
@@ -0,0 +1,42 @@
+﻿/********************************************************************************
+*  ReaderWriterLockExtensions.cs                                                *
+*                                                                               *
+*  Author: Denes Solti                                                          *
+********************************************************************************/
+using System;
+using System.Threading;
+
+namespace Solti.Utils.DI.Internals
+{
+    internal static class ReaderWriterLockExtensions
+    {
+        #region Helpers
+        private sealed class Lock : Disposable
+        {
+            private readonly Action FMReleaseAction;
+
+            public Lock(Action acquireAction, Action releaseAction)
+            {
+                acquireAction();
+                FMReleaseAction = releaseAction;
+            }
+
+            protected override void Dispose(bool disposeManaged)
+            {
+                FMReleaseAction();
+                base.Dispose(disposeManaged);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// író lock elkérése.
+        /// </summary>
+        public static IDisposable AcquireWriterLockSmart(this ReaderWriterLockSlim src) => new Lock(src.EnterWriteLock, src.ExitWriteLock);
+
+        /// <summary>
+        /// Olvasó lock elkérése.
+        /// </summary>
+        public static IDisposable AcquireReaderLockSmart(this ReaderWriterLockSlim src) =>new Lock(src.EnterReadLock, src.ExitReadLock);
+    }
+}
