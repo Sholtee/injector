550879a30a58dd4842cb8fa9e3cac0823b87e4e4
Dénes Solti <sodnaatx@gmail.com>
2019-09-05 08:08:32 +0200

    cover.cmd fix [create the output folder if not exist]

--- cover.cmd
+++ cover.cmd
@@ -15,5 +15,9 @@ if exist "%reports%" rmdir /Q /S "%reports%"
 nuget install ReportGenerator -OutputDirectory %vendor% -Version 4.2.15
 nuget install OpenCover -OutputDirectory %vendor% -Version 4.7.922
 
+if not exist %bin% mkdir %bin%
+
 call "%vendor%\OpenCover.4.7.922\tools\OpenCover.Console.exe" -target:"%ProgramFiles%\dotnet\dotnet.exe" -targetargs:"test %~dp0TEST\Injector.Tests.csproj --configuration:Debug -p:NoDocfx=True" -output:"%coveragexml%" -oldStyle -register:user -filter:"+[Injector*]* -[Injector.Tests]*"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 dotnet "%vendor%\ReportGenerator.4.2.15\tools\netcoreapp2.1\ReportGenerator.dll" "-reports:%coveragexml%" "-targetdir:%reports%"
