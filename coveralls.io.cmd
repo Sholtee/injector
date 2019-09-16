092ac74feec2d167ccd12dd49db2cf99c38b5e93
Dénes Solti <sodnaatx@gmail.com>
2019-09-16 08:00:16 +0200

    coveralls.io II

--- coveralls.io.cmd
+++ coveralls.io.cmd
@@ -20,4 +20,4 @@ if not exist %bin% mkdir %bin%
 call "%vendor%\OpenCover.4.7.922\tools\OpenCover.Console.exe" -target:"%ProgramFiles%\dotnet\dotnet.exe" -targetargs:"test %~dp0TEST\Injector.Tests.csproj --configuration:Debug -p:NoDocfx=True" -output:"%coveragexml%" -oldStyle -register:user -filter:"+[Injector*]* -[Injector.Tests]*"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet "%vendor%\coveralls.io.1.4.2\tools\coveralls.net.exe --opencover %coveragexml% -r %COVERALLS_REPO_TOKEN%
+dotnet %vendor%\coveralls.io.1.4.2\tools\coveralls.net.exe --opencover %coveragexml% -r %COVERALLS_REPO_TOKEN%
