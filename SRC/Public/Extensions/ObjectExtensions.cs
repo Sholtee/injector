00a62de3d069d938715d313f3d017f60de8dfc79
Dénes Solti <sodnaatx@gmail.com>
2019-09-24 11:47:24 +0200

    ObjectExtensions

---
+++ SRC/Public/Extensions/ObjectExtensions.cs
@@ -0,0 +1,71 @@
+﻿/********************************************************************************
+* ObjectExtensions.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI.Proxy
+{
+    using Internals;
+    using Properties;
+
+    /// <summary>
+    /// Defines the base class for duck typing.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Generates duck typing related proxy objects.
+        /// </summary>
+        public class DuckFactory<TTarget>
+        {
+            internal TTarget Target { get; }
+
+            public DuckFactory(TTarget target) => Target = target;
+
+            /// <summary>
+            /// Generates a proxy object to let the <see cref="Target"/> behave like a <typeparamref name="TInterface"/> instance.
+            /// </summary>
+            /// <returns>The newly created proxy object.</returns>
+            public TInterface Like<TInterface>() where TInterface: class
+            {
+                //
+                // Kell egyaltalan proxy-t letrhoznunk?
+                //
+
+                var possibleResult = Target as TInterface;
+                if (possibleResult != null) return possibleResult;
+
+                Type interfaceType = typeof(TInterface);
+
+                if (!interfaceType.IsInterface())
+                    throw new ArgumentException(Resources.NOT_AN_INTERFACE);
+
+                if (interfaceType.ContainsGenericParameters())
+                    throw new ArgumentException(Resources.CANT_INSTANTIATE_GENERICS);
+
+                if (interfaceType.IsNotPublic())
+                    throw new InvalidOperationException(string.Format(Resources.TYPE_NOT_VISIBLE, interfaceType));
+
+                Type targetType = typeof(TTarget);
+
+                if (targetType.IsNotPublic())
+                    throw new InvalidOperationException(string.Format(Resources.TYPE_NOT_VISIBLE, targetType));
+
+                return (TInterface)GeneratedDuck<TInterface, TTarget>.Type.CreateInstance(new[] { typeof(TTarget) }, Target);
+            }
+        }
+
+        /// <summary>
+        /// Marks a <typeparamref name="TTarget"/> instance for duck typing.
+        /// </summary>
+        public static DuckFactory<TTarget> Acts<TTarget>(this TTarget target)
+        {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            return new DuckFactory<TTarget>(target);
+        }
+    }
+}
---
+++ TEST/Proxy/Duck.cs
@@ -0,0 +1,27 @@
+﻿/********************************************************************************
+* Duck.cs                                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.DI.Proxy.Tests
+{
+    using Internals;
+
+    [TestFixture]
+    public sealed class DuckTypingTests
+    {
+        [Test]
+        public void Like_ShouldTryToCastTheTarget()
+        {
+            var disposable = new Disposable();
+            IDisposable result = disposable.Acts().Like<IDisposable>();
+
+            Assert.AreSame(disposable, result);
+        }
+
+    }
+}
