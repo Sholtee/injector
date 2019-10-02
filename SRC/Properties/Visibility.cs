9a8a9ae3704da644407153cda9f2b2f51cea8239
Dénes Solti <sodnaatx@gmail.com>
2019-10-02 06:42:24 +0200

    Visibility.cs cleanup

--- SRC/Properties/Visibility.cs
+++ SRC/Properties/Visibility.cs
@@ -6,11 +6,11 @@
 using System.Runtime.CompilerServices;
 
 [
-assembly: 
-    InternalsVisibleTo("Injector.Tests"), 
-    InternalsVisibleTo("Injector.Perf"), 
-    InternalsVisibleTo("Solti.Utils.DI.Internals.ParameterValidatorProxy<Solti.Utils.DI.IServiceContainer>_Solti.Utils.DI.IServiceContainer_Proxy"),
-    InternalsVisibleTo("Solti.Utils.DI.Internals.ParameterValidatorProxy<Solti.Utils.DI.IInjector>_Solti.Utils.DI.IInjector_Proxy"),
-    InternalsVisibleTo("System.Runtime.Loader.AssemblyLoadContext_Solti.Utils.DI.IAssemblyLoadContext_Duck"),
-    InternalsVisibleTo("DynamicProxyGenAssembly2") // Moq
+assembly:
+#if DEBUG
+    InternalsVisibleTo("Injector.Tests"),
+    InternalsVisibleTo("DynamicProxyGenAssembly2"), // Moq
+#endif
+    InternalsVisibleTo("Injector.Perf"),
+    InternalsVisibleTo("System.Runtime.Loader.AssemblyLoadContext_Solti.Utils.DI.IAssemblyLoadContext_Duck")    
 ]
