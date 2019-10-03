3d1114527df18ac126a04c2e8af4d7cbff3a7b64
Dénes Solti <sodnaatx@gmail.com>
2019-10-03 20:56:35 +0200

    deploying benchmark results II

--- SCRIPTS/gh-pages.cmd
+++ SCRIPTS/gh-pages.cmd
@@ -6,6 +6,12 @@
 @echo off
 
 set /p root=<root
+
+::--------------------------------
+:: prepare repo
+::--------------------------------
+@echo cloning gh-pages...
+
 set repo_dir=%root%\gh-pages
 set docs_branch=gh-pages
 
@@ -16,7 +22,10 @@ if exist %repo_dir% (
 
 git clone https://github.com/sholtee/injector.git --branch %docs_branch% %repo_dir%
 
-@echo deleting the old content...
+::--------------------------------
+:: generate API docs
+::--------------------------------
+@echo generating API docs...
 
 set docs_dir=%repo_dir%\doc
 
@@ -24,31 +33,41 @@ if exist %docs_dir% (
     call rmdir /Q /S %docs_dir%
 )
 
-set perf_dir=%repo_dir%\perf
+call docfx
+move "%root%\doc" "%repo_dir%"
 
-if exist %perf_dir% (
-    call rmdir /Q /S %perf_dir%
+::-----------------------------------------
+:: generate benchmark results (if needed)
+::-----------------------------------------
+set bm_dir="%root%\BenchmarkDotNet.Artifacts"
+
+if exist %bm_dir% (
+  @echo generating benchmark docs...
+
+  set perf_dir=%repo_dir%\perf
+
+  if exist %perf_dir% (
+      call rmdir /Q /S %perf_dir%
+  )
+  
+  call docfx-perf
+  move "%bm_dir%\perf" "%repo_dir%"
 )
 
-@echo calling DocFx...
-
-call docfx
-call docfx-perf
-
-@echo moving the new content...
-
-move "%root%\doc" "%repo_dir%"
-move "%root%\BenchmarkDotNet.Artifacts\perf" "%repo_dir%"
-
-@echo committing changes...
+::--------------------------------
+:: committing changes
+::--------------------------------
+@echo committing...
 
 cd %repo_dir%
 
 git checkout %docs_branch%
 git add doc -A
 git commit -m "docs up"
-git add perf -A
-git commit -m "benchmarks up"
+if exist %bm_dir% (
+  git add perf -A
+  git commit -m "benchmarks up"
+)
 git push origin %docs_branch%
 
 cd %~dp0
