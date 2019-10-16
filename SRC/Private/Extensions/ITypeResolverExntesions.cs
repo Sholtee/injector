a7c209d64f67abe93b0f2bc58e587b7d3f4c17c6
Dénes Solti <sodnaatx@gmail.com>
2019-10-16 15:52:53 +0200

    ITypeResolverExntesions.cs comment

--- SRC/Private/Extensions/ITypeResolverExntesions.cs
+++ SRC/Private/Extensions/ITypeResolverExntesions.cs
@@ -10,9 +10,16 @@ namespace Solti.Utils.DI.Internals
 {
     internal static class ITypeResolverExntesions
     {
-        public static Lazy<Type> AsLazy(this ITypeResolver resolver, Type iface) => Cache<int, Lazy<Type>>.GetOrAdd
+        public static Lazy<Type> AsLazy(this ITypeResolver resolver, Type iface) => Cache<object, Lazy<Type>>.GetOrAdd
         (
-            key: new {resolver, iface}.GetHashCode(), 
+            //
+            // Nem vagom miert de ha "new {resolver, iface}" formaban adom meg a kulcsot
+            // akkor hiaba lesz az anonim oljektum hash kodja azonos parametereknel ugyanaz
+            // (a gyorsitotar implementacioban scope-al egyutt is) a szotarba meg is ket 
+            // kulon bejegyzeskent kerul be.
+            //
+
+            key: new {hk = resolver.GetHashCode(), iface}, 
             factory: () => new Lazy<Type>
             (
                 () => resolver.Resolve(iface),
