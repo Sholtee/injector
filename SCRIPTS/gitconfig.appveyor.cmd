ec9f3ffefbb8707fe743dee1f23490b2e78b7661
Dénes Solti <sodnaatx@gmail.com>
2019-10-17 14:31:58 +0200

    gitconfig.appveyor.cmd

---
+++ SCRIPTS/gitconfig.appveyor.cmd
@@ -0,0 +1,11 @@
+::
+:: gitconfig.appveyor.cmd
+::
+:: Author: Denes Solti
+::
+@echo off
+
+git config --global credential.helper store
+powershell -nologo -noprofile -command "Add-Content '%USERPROFILE%\.git-credentials' 'https://%GITHUB_REPO_TOKEN%:x-oauth-basic@github.com`n'"
+git config --global user.email %GITHUB_EMAIL%
+git config --global user.name "[Bot]"
