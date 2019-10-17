1f7c5b35d22836afb2e965476b65676236fbb291
Dénes Solti <sodnaatx@gmail.com>
2019-10-17 11:39:30 +0200

    test.appveyor.cmd

---
+++ SCRIPTS/test.appveyor.cmd
@@ -0,0 +1,15 @@
+::
+:: test.appveyor.cmd
+::
+:: Author: Denes Solti
+::
+@echo off
+
+if /i "%APPVEYOR_REPO_TAG_NAME%" == "perf" (
+  echo Running performance tests
+  call perf
+) else (
+  echo Running regular tests
+  call test
+  call coveralls.io
+)
