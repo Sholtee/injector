29dbd8b11b921dccfc07b7fda3355441c4164071
Dénes Solti <sodnaatx@gmail.com>
2019-10-18 07:51:45 +0200

    gh-pages.cmd fix [missing setlocal]

--- SCRIPTS/gh-pages.cmd
+++ SCRIPTS/gh-pages.cmd
@@ -5,65 +5,66 @@
 ::
 @echo off
 
+setlocal EnableDelayedExpansion
+
 set /p root=<root
 
 ::--------------------------------
-:: prepare repo
+:: Prepare repo
 ::--------------------------------
-set docs_branch=gh-pages
-set repo_dir="%root%\%docs_branch%"
 
-echo Repo dir: %repo_dir%
+set docs_branch=gh-pages
+set repo_dir=%root%\%docs_branch%
+
+echo Repo dir: "%repo_dir%"
 
 if exist "%repo_dir%" (
   echo Cleanup...
-  rmdir /Q /S %repo_dir%
+  rmdir /Q /S "%repo_dir%"
 )
 
 git clone https://github.com/sholtee/injector.git --branch "%docs_branch%" "%repo_dir%"
 
-set bm_artifacts="%~dp0%root%\BenchmarkDotNet.Artifacts"
+set bm_artifacts=%~dp0%root%\BenchmarkDotNet.Artifacts
 
 ::--------------------------------
-:: generate API docs
+:: Generating docs
 ::--------------------------------
+
 if not exist "%bm_artifacts%" (
-  set docs_dir="%repo_dir%\doc"
+  set docs_dir=%repo_dir%\doc
   
-  echo Generating API docs in "%docs_dir%"...
+  echo Generating API docs in "!docs_dir!"...
 
-  if exist "%docs_dir%" (
+  if exist "!docs_dir!" (
     echo Removing old docs...
-    rmdir /Q /S %docs_dir%
+    rmdir /Q /S "!docs_dir!"
   )
 
   call docfx
   
-  echo Adding new docs: "%root%\doc -> %docs_dir%"...
-  xcopy /e /i /y "%root%\doc" "%docs_dir%"
-  
-::-----------------------------------------
-:: generate benchmark results
-::-----------------------------------------
+  echo Adding new docs: "%root%\doc" -> "!docs_dir!"...
+  xcopy /e /i /y "%root%\doc" "!docs_dir!"
 ) else (
-  set perf_dir="%repo_dir%\perf"
+  set perf_dir=%repo_dir%\perf
   
-  echo Generating benchmark docs in "%perf_dir%"...
+  echo Generating benchmark docs in "!perf_dir!"...
 
-  if exist "%perf_dir%" (
+  if exist "!perf_dir!" (
     echo Removing old docs...
-    rmdir /Q /S %perf_dir%
+    rmdir /Q /S "!perf_dir!"
   )
   
   call docfx-perf
   
-  echo Adding new docs: "%bm_artifacts%\perf -> %perf_dir%"...
-  xcopy /e /i /y  "%bm_artifacts%\perf" "%perf_dir%"
+  echo Adding new docs: "%bm_artifacts%\perf" -> "!perf_dir!"...
+  xcopy /e /i /y  "%bm_artifacts%\perf" "!perf_dir!"
 )
 
 ::--------------------------------
-:: committing changes
+:: Committing changes
 ::--------------------------------
+
 cd %repo_dir%
 
 git checkout %docs_branch%
