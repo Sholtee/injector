8182bb3c3ddf36ca1e51fa004c9ccf3f2eb98c05
Dénes Solti <sodnaatx@gmail.com>
2019-10-20 11:56:52 +0200

    testNcover.cmd: create BIN dir if needed

--- SCRIPTS/testNcover.cmd
+++ SCRIPTS/testNcover.cmd
@@ -12,4 +12,6 @@ set bin=%root%\BIN
 
 nuget install OpenCover -OutputDirectory %vendor% -Version 4.7.922
 
+if not exist %bin% mkdir %bin%
+
 call "%vendor%\OpenCover.4.7.922\tools\OpenCover.Console.exe" -target:"%ProgramFiles%\dotnet\dotnet.exe" -targetargs:"test %root%\TEST\Injector.Tests.csproj --framework:netcoreapp2.2 --configuration:Debug --test-adapter-path:. --logger:nunit;LogFilePath=%bin%\testresults.xml" -output:"%bin%\coverage.xml" -oldStyle -register:user -filter:"+[Injector*]* -[Injector.Tests]*" -excludebyattribute:"*.ExcludeFromCoverage*"
