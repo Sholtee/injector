3780720f78bac165020cb942daafef46c321e3eb
Dénes Solti <sodnaatx@gmail.com>
2019-10-22 09:57:26 +0200

    docfx.ps1

---
+++ SCRIPTS/docfx.ps1
@@ -0,0 +1,18 @@
+#
+# docfx.ps1
+#
+# Author: Denes Solti
+#
+.(".\common.ps1")
+
+function DocFx([Parameter(Position = 0)][string] $jsonpath) {
+  Create-Directory $PROJECT.vendor
+  
+  $result = Exec "nuget.exe" -commandArgs "install docfx.console -OutputDirectory `"$(Resolve-Path $PROJECT.vendor)`" -Version 2.46.0"
+
+  if ($result -Ne 0) {
+    return $result
+  }
+  
+  return Exec (Path-Combine $PROJECT.vendor, "docfx.console.2.46.0", "tools", "docfx.exe" | Resolve-Path) -commandArgs $jsonpath
+}
