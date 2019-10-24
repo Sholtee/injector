c538205a58d7b515fa60817e03bab0204cc27111
Dénes Solti <sodnaatx@gmail.com>
2019-10-24 07:43:40 +0200

    pack.ps1 II

--- SCRIPTS/pack.ps1
+++ SCRIPTS/pack.ps1
@@ -3,5 +3,7 @@
 #
 # Author: Denes Solti
 #
+Remove-Directory $PROJECT.bin
+
 $currentbranch=Exec "git.exe" -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput
 Exec "dotnet.exe" -commandArgs "pack `"$($PROJECT.app | Resolve-Path)`" -c Release /p:CurrentBranch=$($currentbranch)"
