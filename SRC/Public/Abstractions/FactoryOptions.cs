19ed332e3ebccecf8e6507e2b019558a07090722
Dénes Solti <sodnaatx@gmail.com>
2019-11-07 15:51:29 +0100

    FactoryOptions

---
+++ SRC/Public/Abstractions/FactoryOptions.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* FactoryOptions.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI
+{
+    /// <summary>
+    /// Controls the factory during the service creation.
+    /// </summary>
+    [Flags]
+    public enum FactoryOptions
+    {
+        /// <summary>
+        /// Default
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Forces the system to manage the lifetime of the newly created service (if possible).
+        /// </summary>
+        ForceAutoDispose
+    }
+}
