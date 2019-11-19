2d3991e2037f986caa5b3baded3cc553ce77d265
Dénes Solti <sodnaatx@gmail.com>
2019-11-19 08:02:40 +0100

    TypeExtensions.cs refactor

--- SRC/Private/Extensions/Polyfills/TypeExtensions.cs
+++ SRC/Private/Extensions/Polyfills/TypeExtensions.cs
@@ -10,22 +10,22 @@ namespace Solti.Utils.DI.Internals
 {
     internal static partial class TypeExtensions
     {
-        public static bool IsClass(this Type type) => type.GetTypeInfo().IsClass;
+        private static T GetFromTypeInfo<T>(this Type src, Func<TypeInfo, T> getter) => getter(src.GetTypeInfo());
 
-        public static bool IsInterface(this Type type) => type.GetTypeInfo().IsInterface;
+        public static bool IsClass(this Type type) => type.GetFromTypeInfo(ti => ti.IsClass);
 
-        public static bool IsGenericType(this Type type) => type.GetTypeInfo().IsGenericType;
+        public static bool IsInterface(this Type type) => type.GetFromTypeInfo(ti => ti.IsInterface);
 
-        public static bool IsGenericTypeDefinition(this Type type) => type.GetTypeInfo().IsGenericTypeDefinition;
+        public static bool IsGenericType(this Type type) => type.GetFromTypeInfo(ti => ti.IsGenericType);
 
-        public static bool IsAbstract(this Type type) => type.GetTypeInfo().IsAbstract;
+        public static bool IsGenericTypeDefinition(this Type type) => type.GetFromTypeInfo(ti => ti.IsGenericTypeDefinition);
 
-        public static Assembly Assembly(this Type type) => type.GetTypeInfo().Assembly;
+        public static bool IsAbstract(this Type type) => type.GetFromTypeInfo(ti => ti.IsAbstract);
 
-        public static bool ContainsGenericParameters(this Type type) => type.GetTypeInfo().ContainsGenericParameters;
+        public static Assembly Assembly(this Type type) => type.GetFromTypeInfo(ti => ti.Assembly);
 
-        public static bool IsSealed(this Type type) => type.GetTypeInfo().IsSealed;
+        public static bool ContainsGenericParameters(this Type type) => type.GetFromTypeInfo(ti => ti.ContainsGenericParameters);
 
-        public static bool IsVisible(this Type type) => type.GetTypeInfo().IsVisible;
+        public static bool IsSealed(this Type type) => type.GetFromTypeInfo(ti => ti.IsSealed);
     }
 }
