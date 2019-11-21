88146aeb2f2fe19d0109db95cd32fa1b5b002944
Dénes Solti <sodnaatx@gmail.com>
2019-11-21 15:34:10 +0100

    DisposeByRefObject

---
+++ SRC/Private/Abstractions/IReferenceCounted.cs
@@ -0,0 +1,13 @@
+﻿/********************************************************************************
+* IReferenceCounted.cs                                                          *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+namespace Solti.Utils.DI.Internals
+{
+    internal interface IReferenceCounted
+    {
+        int AddRef();
+        int Release();
+    }
+}
---
+++ SRC/Private/Patterns/DisposeByRefObject.cs
@@ -0,0 +1,45 @@
+﻿/********************************************************************************
+* DisposeByRefObject.cs                                                         *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Threading;
+
+namespace Solti.Utils.DI.Internals
+{
+    /// <summary>
+    /// Manages object lifetime by refence counting.
+    /// </summary>
+    public class DisposeByRefObject : Disposable, IReferenceCounted
+    {
+        private int FRefCount = 1;
+
+        /// <summary>
+        /// The current reference count.
+        /// </summary>
+        public int RefCount => FRefCount;
+
+        /// <summary>
+        /// Increments the reference counter.
+        /// </summary>
+        /// <returns>The current reference count.</returns>
+        public int AddRef()
+        {
+            CheckDisposed();
+            return Interlocked.Increment(ref FRefCount);
+        }
+
+        /// <summary>
+        /// Decrements the reference counter.
+        /// </summary>
+        /// <remarks>If reference count reaches the zero the object will be disposed.</remarks>
+        /// <returns>The current reference count.</returns>
+        public int Release()
+        {
+            CheckDisposed();
+            int current = Interlocked.Decrement(ref FRefCount);
+            if (current == 0) Dispose();
+            return current;
+        }
+    }
+}
