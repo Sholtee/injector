399e6e289ab8b91eb8f62475fab1918e796a8d16
Dénes Solti <sodnaatx@gmail.com>
2019-11-28 18:59:42 +0100

    ServiceReference

---
+++ SRC/Private/Abstractions/ServiceReference.cs
@@ -0,0 +1,66 @@
+﻿/********************************************************************************
+* ServiceReference.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace Solti.Utils.DI.Internals
+{
+    /// <summary>
+    /// Encapsulates a service and its dependencies into a reference counted container.
+    /// </summary>
+    public class ServiceReference: DisposeByRefObject
+    {
+        /// <summary>
+        /// Creates a new <see cref="ServiceReference"/> instance.
+        /// </summary>
+        /// <param name="instance">The instance to make referenced.</param>
+        /// <param name="dependencies">The dependencies of the instance.</param>
+        public ServiceReference(object instance, IReadOnlyCollection<ServiceReference> dependencies) 
+        {
+            Instance = instance ?? throw new ArgumentNullException(nameof(instance));
+            Dependencies = dependencies ?? throw new ArgumentNullException(nameof(dependencies));
+
+            foreach (ServiceReference dependency in dependencies)
+                dependency.AddRef();
+        }
+
+        /// <summary>
+        /// The referenced service instance.
+        /// </summary>
+        public object Instance { get; }
+
+        /// <summary>
+        /// The dependencies of this service <see cref="Instance"/>.
+        /// </summary>
+        public IReadOnlyCollection<ServiceReference> Dependencies { get; }
+
+        /// <summary>
+        /// Disposes the referenced service <see cref="Instance"/> and decrements the reference counter of all the <see cref="Dependencies"/>.
+        /// </summary>
+        protected override void Dispose(bool disposeManaged)
+        {
+            if (disposeManaged)
+            {
+                //
+                // Elso helyen szerepeljen h a fuggosegeket a Dispose()-ban meg hasznalni tudja a szerviz peldany.
+                //
+
+                (Instance as IDisposable)?.Dispose();
+
+                //
+                // Ha a GC-nek kell felszabaditania a ServiceReference peldanyt akkor vmit nagyon elkurtunk
+                // es mar semmi ertelme a referencia szamlalasnak -> ezert a lenti sor csak "disposeManaged"
+                // eseten ertelmezett.
+                //
+
+                foreach (ServiceReference dependency in Dependencies)
+                    dependency.Release();
+            }
+
+            base.Dispose(disposeManaged);
+        }
+    }
+}
---
+++ TEST/ServiceReference.cs
@@ -0,0 +1,39 @@
+﻿/********************************************************************************
+* ServiceReference.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.DI.Internals.Tests
+{
+    using Internals;
+
+    [TestFixture]
+    public sealed class ServiceReferenceTests
+    {
+        [Test]
+        public void ServiceReference_ShouldManageTheReferenceCounts()
+        {
+            var target = new Disposable();
+            var reference = new ServiceReference(target, Array.Empty<ServiceReference>());
+
+            Assert.That(reference.RefCount, Is.EqualTo(1));
+
+            var svc = new ServiceReference(new object(), new[] { reference });
+            Assert.That(reference.RefCount, Is.EqualTo(2));
+            
+            svc.Release();
+
+            Assert.That(svc.Disposed);
+            Assert.That(reference.RefCount, Is.EqualTo(1));
+
+            reference.Release();
+
+            Assert.That(reference.Disposed);
+            Assert.That(target.Disposed);
+        }
+    }
+}
