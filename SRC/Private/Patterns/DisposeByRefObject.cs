eee72aa7a3d23bf0cf8f741e1a6eb1c22c0e2d2c
Dénes Solti <sodnaatx@gmail.com>
2019-11-28 09:40:37 +0100

    DisposeByRefObject refactor

--- SRC/Private/Patterns/DisposeByRefObject.cs
+++ SRC/Private/Patterns/DisposeByRefObject.cs
@@ -3,9 +3,6 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-using System;
-using System.Threading;
-
 namespace Solti.Utils.DI.Internals
 {
     /// <summary>
@@ -13,29 +10,12 @@ namespace Solti.Utils.DI.Internals
     /// </summary>
     public class DisposeByRefObject : Disposable, IReferenceCounted
     {
-        private int FRefCount = 1;
-
-        /// <summary>
-        /// See <see cref="Disposable.Dispose(bool)"/>
-        /// </summary>
-        protected override void Dispose(bool disposeManaged)
-        {
-            Interlocked.Exchange(ref FRefCount, 0);
-            base.Dispose(disposeManaged);
-        }
-
-        /// <summary>
-        /// See <see cref="Disposable"/>.
-        /// </summary>
-        protected new void CheckDisposed() 
-        {
-            if (FRefCount == 0) throw new ObjectDisposedException(null);
-        }
+        private readonly object FLock = new object();
 
         /// <summary>
         /// The current reference count.
         /// </summary>
-        public int RefCount => FRefCount;
+        public int RefCount { get; private set; } = 1;
 
         /// <summary>
         /// Increments the reference counter.
@@ -43,8 +23,11 @@ namespace Solti.Utils.DI.Internals
         /// <returns>The current reference count.</returns>
         public int AddRef()
         {
-            CheckDisposed();
-            return Interlocked.Increment(ref FRefCount);
+            lock (FLock)
+            {
+                CheckDisposed();
+                return ++RefCount;
+            }
         }
 
         /// <summary>
@@ -54,10 +37,12 @@ namespace Solti.Utils.DI.Internals
         /// <returns>The current reference count.</returns>
         public int Release()
         {
-            CheckDisposed();
-            int current = Interlocked.Decrement(ref FRefCount);
-            if (current == 0) Dispose();
-            return current;
+            lock (FLock)
+            {
+                CheckDisposed();
+                if (--RefCount == 0) Dispose();
+                return RefCount;
+            }
         }
     }
 }
