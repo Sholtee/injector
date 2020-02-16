a8dd01888cf3ffe606d9e4ea57f2b46a4d7e3bc6
Dénes Solti <sodnaatx@gmail.com>
2020-02-16 13:50:04 +0100

    added IServiceProviderExtensions class

---
+++ SRC/Public/Extensions/MS_DI/IServiceProviderExtensions.cs
@@ -0,0 +1,22 @@
+﻿/********************************************************************************
+* IServiceProviderExtensions.cs                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI
+{
+    /// <summary>
+    /// Defines several handy extensions for the <see cref="IServiceProvider"/> interface.
+    /// </summary>
+    public static class IServiceProviderExtensions
+    {
+        /// <summary>
+        /// Gets the service instance associated with the given interface.
+        /// </summary>
+        public static TInterface GetService<TInterface>(this IServiceProvider self) => self != null 
+            ? (TInterface) self.GetService(typeof(TInterface)) 
+            : throw new ArgumentNullException(nameof(self));
+    }
+}
