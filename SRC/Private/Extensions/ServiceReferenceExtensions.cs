1a9f50ddb3337f2672a3e6d92a79527a084aa886
Dénes Solti <sodnaatx@gmail.com>
2020-02-19 20:01:33 +0100

    ServiceReferenceExtensions.SetInstance(): assert()

--- SRC/Private/Extensions/ServiceReferenceExtensions.cs
+++ SRC/Private/Extensions/ServiceReferenceExtensions.cs
@@ -4,11 +4,16 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Solti.Utils.DI.Internals
 {
     internal static class ServiceReferenceExtensions
     {
-        public static bool SetInstance(this ServiceReference svc, IReadOnlyDictionary<string, object> options) => svc.RelatedServiceEntry.SetInstance(svc, options);
+        public static void SetInstance(this ServiceReference svc, IReadOnlyDictionary<string, object> options)
+        {
+            bool succeeded = svc.RelatedServiceEntry.SetInstance(svc, options);
+            Debug.Assert(succeeded, $"{nameof(SetInstance)}() failed");
+        }
     }
 }
