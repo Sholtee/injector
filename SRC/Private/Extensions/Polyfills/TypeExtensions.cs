033ae6dc1eb598e35cf61422102c52b272c85f46
Dénes Solti <sodnaatx@gmail.com>
2020-03-24 17:00:56 +0100

    TypeExtensions.cs: extensions should be inlined

--- SRC/Private/Extensions/Polyfills/TypeExtensions.cs
+++ SRC/Private/Extensions/Polyfills/TypeExtensions.cs
@@ -5,6 +5,7 @@
 ********************************************************************************/
 using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Solti.Utils.DI.Internals
 {
@@ -12,16 +13,22 @@ namespace Solti.Utils.DI.Internals
     {
         private static T GetFromTypeInfo<T>(this Type src, Func<TypeInfo, T> getter) => getter(src.GetTypeInfo());
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsClass(this Type type) => type.GetFromTypeInfo(ti => ti.IsClass);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsAbstract(this Type type) => type.GetFromTypeInfo(ti => ti.IsAbstract);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsInterface(this Type type) => type.GetFromTypeInfo(ti => ti.IsInterface);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsGenericType(this Type type) => type.GetFromTypeInfo(ti => ti.IsGenericType);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsGenericTypeDefinition(this Type type) => type.GetFromTypeInfo(ti => ti.IsGenericTypeDefinition);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Assembly Assembly(this Type type) => type.GetFromTypeInfo(ti => ti.Assembly);
     }
 }
