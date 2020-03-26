d2243288a2c2f381c2d0a31ce949a1fc0c4f956b
Dénes Solti <sodnaatx@gmail.com>
2020-03-26 19:03:17 +0100

    test performance of Injector creation/disposal

---
+++ PERF/InjectorLifetime.cs
@@ -0,0 +1,51 @@
+﻿/********************************************************************************
+* InjectorLifetime.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.DI.Perf
+{
+    using static Consts;
+
+    [MemoryDiagnoser]
+    public class InjectorLifetime
+    {
+        private static readonly IReadOnlyList<Type> RandomInterfaces = typeof(object)
+            .Assembly
+            .GetTypes()
+            .Where(t => t.IsInterface)
+            .ToArray();
+
+        [Params(0, 10, 100)]
+        public int ServiceCount { get; set; }
+
+        private IServiceContainer FContainer;
+
+        [GlobalSetup]
+        public void Setup() 
+        {
+            FContainer = new DI.ServiceContainer();
+
+            for (int i = 0; i < ServiceCount; i++)
+                FContainer.Factory(RandomInterfaces[i % RandomInterfaces.Count], (i / RandomInterfaces.Count).ToString(), (i, t) => null, (Lifetime) (i % (int) Lifetime.Singleton));
+        }
+
+        [GlobalCleanup]
+        public void Cleanup() => FContainer.Dispose();
+
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void CreateThenDispose() 
+        {
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                using (FContainer.CreateInjector()) { }
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -1,7 +1,7 @@
 # Injector.NET Benchmark Results
 
 - [Activator](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.Activator-report-github.html )
-- [Composite](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.Composite-report-github.html )
 - [Injector](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.Injector-report-github.html )
+- [Injector Lifetime](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.InjectorLifetime-report-github.html )
 - [Lazy Injector](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.LazyInjector-report-github.html )
 - [ServiceContainer](https://sholtee.github.io/injector/perf/Solti.Utils.DI.Perf.ServiceContainer-report-github.html )
