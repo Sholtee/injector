8a821e5c128328b3948f06205b32aa3c75208957
Dénes Solti <sodnaatx@gmail.com>
2020-04-04 13:09:04 +0200

    clean up IServiceContainerExtensions.cs

--- SRC/Public/Extensions/IServiceContainerExtensions/IServiceContainerExtensions.cs
+++ SRC/Public/Extensions/IServiceContainerExtensions/IServiceContainerExtensions.cs
@@ -3,12 +3,8 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-using System;
-
 namespace Solti.Utils.DI
 {
-    using Internals;
-
     /// <summary>
     /// Defines several handy extensions for the <see cref="IServiceContainer"/> interface.
     /// </summary>
