492ba8d015dcc3da4951d4847a186da66d12e5f2
Dénes Solti <sodnaatx@gmail.com>
2020-04-13 08:33:58 +0200

    IServiceContainerExtensions.Provider() docs

--- SRC/Public/Extensions/IServiceContainerExtensions/Provider.cs
+++ SRC/Public/Extensions/IServiceContainerExtensions/Provider.cs
@@ -12,14 +12,14 @@ namespace Solti.Utils.DI
     public static partial class IServiceContainerExtensions
     {
         /// <summary>
-        /// 
+        /// Registers a new provider. Providers are "factory services" responsible for creating the concrete service.
         /// </summary>
-        /// <param name="self"></param>
-        /// <param name="iface"></param>
-        /// <param name="name"></param>
-        /// <param name="provider"></param>
-        /// <param name="lifetime"></param>
-        /// <returns></returns>
+        /// <param name="self">The target <see cref="IServiceContainer"/>.</param>
+        /// <param name="iface">The interface of the service. It will be forwarded to the provider.</param>
+        /// <param name="name">The (optional) name of the service.</param>
+        /// <param name="provider">The type of the provider. It may have dependencies and must implement the <see cref="IServiceProvider"/> interface.</param>
+        /// <param name="lifetime">The <see cref="Lifetime"/> of the service.</param>
+        /// <returns>The container itself.</returns>
         public static IServiceContainer Provider(this IServiceContainer self, Type iface, string? name, Type provider, Lifetime lifetime = Lifetime.Transient)
         {
             Ensure.Parameter.IsNotNull(self, nameof(self));
@@ -56,35 +56,35 @@ namespace Solti.Utils.DI
         }
 
         /// <summary>
-        /// 
+        /// Registers a new provider. Providers are "factory services" responsible for creating the concrete service.
         /// </summary>
-        /// <param name="self"></param>
-        /// <param name="iface"></param>
-        /// <param name="provider"></param>
-        /// <param name="lifetime"></param>
-        /// <returns></returns>
+        /// <param name="self">The target <see cref="IServiceContainer"/>.</param>
+        /// <param name="iface">The interface of the service. It will be forwarded to the provider.</param>
+        /// <param name="provider">The type of the provider. It may have dependencies and must implement the <see cref="IServiceProvider"/> interface.</param>
+        /// <param name="lifetime">The <see cref="Lifetime"/> of the service.</param>
+        /// <returns>The container itself.</returns>
         public static IServiceContainer Provider(this IServiceContainer self, Type iface, Type provider, Lifetime lifetime = Lifetime.Transient) => self.Provider(iface, null, provider, lifetime);
 
         /// <summary>
-        /// 
+        /// Registers a new provider. Providers are "factory services" responsible for creating the concrete service.
         /// </summary>
-        /// <typeparam name="TInterface"></typeparam>
-        /// <typeparam name="TProvider"></typeparam>
-        /// <param name="self"></param>
-        /// <param name="name"></param>
-        /// <param name="lifetime"></param>
-        /// <returns></returns>
+        /// <typeparam name="TInterface">The interface of the service. It will be forwarded to the provider.</typeparam>
+        /// <typeparam name="TProvider">The type of the provider. It may have dependencies and must implement the <see cref="IServiceProvider"/> interface.</typeparam>
+        /// <param name="self">The target <see cref="IServiceContainer"/>.</param>
+        /// <param name="name">The (optional) name of the service.</param>
+        /// <param name="lifetime">The <see cref="Lifetime"/> of the service.</param>
+        /// <returns>The container itself.</returns>
         public static IServiceContainer Provider<TInterface, TProvider>(this IServiceContainer self, string? name, Lifetime lifetime = Lifetime.Transient) where TProvider : class, IServiceProvider where TInterface : class
             => self.Provider(typeof(TInterface), name, typeof(TProvider), lifetime);
 
         /// <summary>
-        /// 
+        /// Registers a new provider. Providers are "factory services" responsible for creating the concrete service.
         /// </summary>
-        /// <typeparam name="TInterface"></typeparam>
-        /// <typeparam name="TProvider"></typeparam>
-        /// <param name="self"></param>
-        /// <param name="lifetime"></param>
-        /// <returns></returns>
+        /// <typeparam name="TInterface">The interface of the service. It will be forwarded to the provider.</typeparam>
+        /// <typeparam name="TProvider">The type of the provider. It may have dependencies and must implement the <see cref="IServiceProvider"/> interface.</typeparam>
+        /// <param name="self">The target <see cref="IServiceContainer"/>.</param>
+        /// <param name="lifetime">The <see cref="Lifetime"/> of the service.</param>
+        /// <returns>The container itself.</returns>
         public static IServiceContainer Provider<TInterface, TProvider>(this IServiceContainer self, Lifetime lifetime = Lifetime.Transient) where TProvider : class, IServiceProvider where TInterface : class
             => self.Provider<TInterface, TProvider>(null, lifetime);
     }
