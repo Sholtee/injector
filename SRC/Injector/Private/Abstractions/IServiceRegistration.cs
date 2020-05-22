0e1d87226c6462386a24fa34cc120e065c195864
Dénes Solti <sodnaatx@gmail.com>
2020-05-22 10:07:10 +0200

    service registration attributes moved to interfaces

---
+++ SRC/Injector/Private/Abstractions/IServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿/********************************************************************************
+* IServiceRegistration.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI.Internals
+{
+    using Interfaces;
+    
+    internal interface IServiceRegistration
+    {
+        void Invoke(IServiceContainer container, ServiceRegistrationAttribute registration, Type referencedType);
+    }
+}
---
+++ SRC/Injector/Private/ProviderRegistration.cs
@@ -0,0 +1,20 @@
+﻿/********************************************************************************
+* ProviderRegistration.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.DI.Internals
+{
+    using Interfaces;
+
+    internal sealed class ProviderRegistration : IServiceRegistration
+    {
+        public void Invoke(IServiceContainer container, ServiceRegistrationAttribute registration, Type referencedType) => container.Provider(
+            registration.Interface, 
+            registration.Name, 
+            referencedType, 
+            registration.Lifetime!.Value);
+    }
+}
