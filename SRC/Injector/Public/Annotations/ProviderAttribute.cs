655036b8ccfef113a931bac5f9498d3a553a5458
Dénes Solti <sodnaatx@gmail.com>
2020-05-22 10:16:05 +0200

    missing forwards

---
+++ SRC/Injector/Public/Annotations/ProviderAttribute.cs
@@ -0,0 +1,9 @@
+﻿/********************************************************************************
+* ProviderAttribute.cs                                                          *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using Solti.Utils.DI.Interfaces;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(ProviderAttribute))]
---
+++ SRC/Injector/Public/Annotations/ServiceRegistrationAttribute.cs
@@ -0,0 +1,9 @@
+﻿/********************************************************************************
+* ServiceRegistrationAttribute.cs                                               *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using Solti.Utils.DI.Interfaces;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(ServiceRegistrationAttribute))]
