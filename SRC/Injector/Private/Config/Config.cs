0540ff1a18cdd30982b9eaa315be814b0f40d98c
Dénes Solti <sodnaatx@gmail.com>
2020-08-26 15:05:42 +0200

    Config.CreateInstance() fix [config could not be modified via the config.json]

--- SRC/Injector/Private/Config/Config.cs
+++ SRC/Injector/Private/Config/Config.cs
@@ -18,7 +18,7 @@ namespace Solti.Utils.DI.Internals
     {
         private static Config CreateInstance()
         {
-            string configFile = Path.ChangeExtension(typeof(Config).Assembly.Location, "Solti.Utils.DI.config.json");
+            string configFile = Path.Combine(Path.GetDirectoryName(typeof(Config).Assembly.Location), $"{typeof(Config).Assembly.GetName().Name}.config.json");
 
             if (File.Exists(configFile))
                 return JsonSerializer.Deserialize<Config>(File.ReadAllText(configFile));
