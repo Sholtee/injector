366b79c25c777d350a3d9a365ea25b7a3365ccab
Dénes Solti <sodnaatx@gmail.com>
2020-08-26 14:12:12 +0200

    add some facades

---
+++ SRC/Injector/Public/Extensions/IServiceContainerBasicExtensions.cs
@@ -0,0 +1,9 @@
+/********************************************************************************
+* IServiceContainerBasicExtensions.cs                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Runtime.CompilerServices;
+using Solti.Utils.DI.Interfaces;
+
+[assembly: TypeForwardedTo(typeof(IServiceContainerBasicExtensions))]
