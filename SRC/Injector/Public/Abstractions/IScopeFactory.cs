b7d72cde5fda3fe4ab7086e7abe9ba2817d3f11d
Dénes Solti <sodnaatx@gmail.com>
2021-02-13 09:49:07 +0100

    missing type forwards

---
+++ SRC/Injector/Public/Abstractions/IScopeFactory.cs
@@ -0,0 +1,9 @@
+﻿/********************************************************************************
+* IScopeFactory.cs                                                              *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using Solti.Utils.DI.Interfaces;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(IScopeFactory))]
