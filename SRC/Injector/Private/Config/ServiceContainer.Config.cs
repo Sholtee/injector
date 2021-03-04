0e2ad141bb7f20e19766930e2ae7f5077c1850fd
Dénes Solti <sodnaatx@gmail.com>
2021-03-04 07:13:44 +0100

    ServiceContainer.Config.cs: fix documentation

--- SRC/Injector/Private/Config/ServiceContainer.Config.cs
+++ SRC/Injector/Private/Config/ServiceContainer.Config.cs
@@ -5,17 +5,15 @@
 ********************************************************************************/
 namespace Solti.Utils.DI.Internals
 {
-    using Primitives.Patterns;
-
     public partial class Config 
     {
         /// <summary>
-        /// Defines several options to control the behavior of the <see cref="IComposite{T}"/> objects.
+        /// Defines some options that control the behavior of the <see cref="DI.ServiceContainer"/> objects.
         /// </summary>
         public class ServiceContainerConfig
         {
             /// <summary>
-            /// Limits the count of children belong to a <see cref="IComposite{T}"/> entity.
+            /// Limits the count of the nested containers.
             /// </summary>
             public int MaxChildCount { get; set; } = GetValue<int?>("ServiceContainer.MaxChildCount") ?? 512;
         }
