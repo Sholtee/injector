a6b6a8cf1cd7e762534a3e5e638239a26cf1126e
Dénes Solti <sodnaatx@gmail.com>
2021-06-01 09:08:14 +0200

    not shared entries don't need ExclusiveBlock

--- SRC/Injector/Private/ServiceEntries/PooledServiceEntry.cs
+++ SRC/Injector/Private/ServiceEntries/PooledServiceEntry.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Solti.Utils.DI.Internals
 {
@@ -18,22 +17,6 @@ namespace Solti.Utils.DI.Internals
     internal class PooledServiceEntry : ProducibleServiceEntryBase, ISupportsSpecialization
     {
         private readonly List<IServiceReference> FInstances = new(1); // max egy eleme lehet
-
-        private readonly ExclusiveBlock FExclusiveBlock = new();
-
-        protected override void Dispose(bool disposeManaged)
-        {
-            if (disposeManaged)
-                FExclusiveBlock.Dispose();
-
-            base.Dispose(disposeManaged);
-        }
-
-        protected override async ValueTask AsyncDispose()
-        {
-            await FExclusiveBlock.DisposeAsync();
-            await base.AsyncDispose();
-        }
 
         protected PooledServiceEntry(PooledServiceEntry entry, IServiceContainer owner) : base(entry, owner)
         {
@@ -53,71 +36,68 @@ namespace Solti.Utils.DI.Internals
 
         public override bool SetInstance(IServiceReference reference)
         {
-            using (FExclusiveBlock.Enter())
+            EnsureAppropriateReference(reference);
+            EnsureProducible();
+
+            IInjector relatedInjector = Ensure.IsNotNull(reference.RelatedInjector, $"{nameof(reference)}.{nameof(reference.RelatedInjector)}");
+            Ensure.AreEqual(relatedInjector.UnderlyingContainer, Owner, Resources.INAPPROPRIATE_OWNERSHIP);
+
+            //
+            // Ha mar le lett gyartva akkor nincs dolgunk, jelezzuk a hivonak h ovlassa ki a
+            // korabban beallitott erteket -> Kovetkezes kepp egy scope MINDIG csak egy 
+            // elemet vehet ki a pool-bol
+            //
+
+            if (State.HasFlag(ServiceEntryStates.Built)) return false;
+
+            //
+            // Pool-ban az eredeti factory-t hivjuk
+            //
+
+            if (relatedInjector.GetOption<bool>(PooledLifetime.POOL_SCOPE))
             {
-                EnsureAppropriateReference(reference);
-                EnsureProducible();
-
-                IInjector relatedInjector = Ensure.IsNotNull(reference.RelatedInjector, $"{nameof(reference)}.{nameof(reference.RelatedInjector)}");
-                Ensure.AreEqual(relatedInjector.UnderlyingContainer, Owner, Resources.INAPPROPRIATE_OWNERSHIP);
-
-                //
-                // Ha mar le lett gyartva akkor nincs dolgunk, jelezzuk a hivonak h ovlassa ki a
-                // korabban beallitott erteket -> Kovetkezes kepp egy scope MINDIG csak egy 
-                // elemet vehet ki a pool-bol
-                //
-
-                if (State.HasFlag(ServiceEntryStates.Built)) return false;
-
-                //
-                // Pool-ban az eredeti factory-t hivjuk
-                //
-
-                if (relatedInjector.GetOption<bool>(PooledLifetime.POOL_SCOPE))
-                {
-                    reference.Value = Factory!(relatedInjector, Interface);
-                }
-
-                //
-                // Fogyasztoban megszolitjuk a bejegyzeshez tartozo PoolService-t
-                //
-
-                else
-                {
-                    IPool relatedPool = (IPool) relatedInjector.Get(typeof(IPool<>).MakeGenericType(Interface), PooledLifetime.GetPoolName(Interface, Name));
-
-                    //
-                    // Mivel a mogottes ObjectPool<>.Get() ugyanazt az entitast adja vissza ha ugyanabbol a szalbol tobbszor
-                    // hivjuk (es a szerviz maga hiaba Scoped ez siman lehetseges ha tobb injector-t hozunk letre ugyanabban
-                    // a szalban). Ezert h a felszabaditassal ne legyen kavarodas ilyen esetben kivetelt dobunk.
-                    //
-
-                    if (relatedPool.Any(item => item.OwnerThread == Thread.CurrentThread.ManagedThreadId))
-                        throw new RequestNotAllowedException(Resources.POOL_ITEM_ALREADY_TAKEN);
-
-                    PoolItem<IServiceReference> poolItem = relatedPool.Get(CheckoutPolicy.Block)!; // CheckoutPolicy.Block miatt sose NULL
-
-                    //
-                    // Mivel a pool elem scope-ja kulonbozik "relatedInjector" scope-jatol (egymastol fuggetlenul 
-                    // felszabaditasra kerulhetnek) ezert felvesszuk az elemet fuggosegkent is h biztosan ne
-                    // legyen gond az elettartammal.
-                    //
-
-                    relatedInjector.Get<IServicePath>().Requestor?.AddDependency(poolItem.Value);
-
-                    //
-                    // Nem gond h a poolItem-et adjuk vissza, igy NEM annak tartalma kerul felszabaditasra a 
-                    // scope lezarasakor (poolItem felszabaditasa visszateszi a legyartott elemet a pool-ba).
-                    //
-
-                    reference.Value = poolItem;
-                }
-
-                FInstances.Add(reference);
-                State |= ServiceEntryStates.Built;
-
-                return true;
+                reference.Value = Factory!(relatedInjector, Interface);
             }
+
+            //
+            // Fogyasztoban megszolitjuk a bejegyzeshez tartozo PoolService-t
+            //
+
+            else
+            {
+                IPool relatedPool = (IPool) relatedInjector.Get(typeof(IPool<>).MakeGenericType(Interface), PooledLifetime.GetPoolName(Interface, Name));
+
+                //
+                // Mivel a mogottes ObjectPool<>.Get() ugyanazt az entitast adja vissza ha ugyanabbol a szalbol tobbszor
+                // hivjuk (es a szerviz maga hiaba Scoped ez siman lehetseges ha tobb injector-t hozunk letre ugyanabban
+                // a szalban). Ezert h a felszabaditassal ne legyen kavarodas ilyen esetben kivetelt dobunk.
+                //
+
+                if (relatedPool.Any(item => item.OwnerThread == Thread.CurrentThread.ManagedThreadId))
+                    throw new RequestNotAllowedException(Resources.POOL_ITEM_ALREADY_TAKEN);
+
+                PoolItem<IServiceReference> poolItem = relatedPool.Get(CheckoutPolicy.Block)!; // CheckoutPolicy.Block miatt sose NULL
+
+                //
+                // Mivel a pool elem scope-ja kulonbozik "relatedInjector" scope-jatol (egymastol fuggetlenul 
+                // felszabaditasra kerulhetnek) ezert felvesszuk az elemet fuggosegkent is h biztosan ne
+                // legyen gond az elettartammal.
+                //
+
+                relatedInjector.Get<IServicePath>().Requestor?.AddDependency(poolItem.Value);
+
+                //
+                // Nem gond h a poolItem-et adjuk vissza, igy NEM annak tartalma kerul felszabaditasra a 
+                // scope lezarasakor (poolItem felszabaditasa visszateszi a legyartott elemet a pool-ba).
+                //
+
+                reference.Value = poolItem;
+            }
+
+            FInstances.Add(reference);
+            State |= ServiceEntryStates.Built;
+
+            return true;
         }
 
         public sealed override AbstractServiceEntry CopyTo(IServiceContainer target)
