bc8f43d12e91f29e70b63cc09886fe8eaabaf144
Dénes Solti <sodnaatx@gmail.com>
2021-09-10 09:48:29 +0200

    updated IocComparison tests

--- PERF/IocComparison.cs
+++ PERF/IocComparison.cs
@@ -18,6 +18,8 @@ namespace Solti.Utils.DI.Perf
     [SimpleJob(RunStrategy.Throughput, invocationCount: 30000)]
     public partial class IocComparison
     {
+        const int RequestPerInvoke = 50;
+
         #region Services
         public interface IDependency
         {
@@ -49,11 +51,14 @@ namespace Solti.Utils.DI.Perf
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private IDependant RequestService()
+        private void RequestService()
         {
             using (Container.CreateScope(out IServiceProvider serviceProvider))
             {
-                return serviceProvider.GetService<IDependant>();
+                for (int i = 0; i < RequestPerInvoke; i++)
+                {
+                    _ = serviceProvider.GetService<IDependant>();
+                }
             }
         }
 
@@ -80,23 +85,23 @@ namespace Solti.Utils.DI.Perf
             .RegisterSingleton<IDependant, Dependant>()
             .Build();
 
-        [Benchmark]
-        public IDependant SingletonService() => RequestService();
+        [Benchmark(OperationsPerInvoke = RequestPerInvoke)]
+        public void SingletonService() => RequestService();
 
         [GlobalSetup(Target = nameof(ScopedService))]
         public void SetupScopedService() => Container
             .RegisterScoped<IDependency, Dependency>()
             .RegisterScoped<IDependant, Dependant>().Build();
 
-        [Benchmark]
-        public IDependant ScopedService() => RequestService();
+        [Benchmark(OperationsPerInvoke = RequestPerInvoke)]
+        public void ScopedService() => RequestService();
 
         [GlobalSetup(Target = nameof(TransientService))]
         public void SetupTransientService() => Container
             .RegisterTransient<IDependency, Dependency>()
             .RegisterTransient<IDependant, Dependant>().Build();
 
-        [Benchmark]
-        public IDependant TransientService() => RequestService();
+        [Benchmark(OperationsPerInvoke = RequestPerInvoke)]
+        public void TransientService() => RequestService();
     }
 }
