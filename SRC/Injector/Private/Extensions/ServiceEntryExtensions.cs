5164b026d7be4e262e299390d664289a643d5615
Dénes Solti <sodnaatx@gmail.com>
2021-09-20 08:51:04 +0200

    ServiceEntryExtensions.cs cleanup

--- SRC/Injector/Private/Extensions/ServiceEntryExtensions.cs
+++ SRC/Injector/Private/Extensions/ServiceEntryExtensions.cs
@@ -5,10 +5,7 @@
 ********************************************************************************/
 namespace Solti.Utils.DI.Internals
 {
-    using Interfaces;
-
     internal static partial class ServiceEntryExtensions
     {
-        public static bool IsGeneric(this AbstractServiceEntry entry) => entry.Interface.IsGenericTypeDefinition;
     }
 }
