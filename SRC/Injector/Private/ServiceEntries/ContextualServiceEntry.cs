c39dbd65578f57f35fe304ab53c3b34c1731b62c
Dénes Solti <sodnaatx@gmail.com>
2021-09-25 09:30:04 +0200

    ContextualServiceEntry.cs refactor

--- SRC/Injector/Private/ServiceEntries/ContextualServiceEntry.cs
+++ SRC/Injector/Private/ServiceEntries/ContextualServiceEntry.cs
@@ -11,25 +11,28 @@ namespace Solti.Utils.DI.Internals
 
     internal sealed class ContextualServiceEntry : AbstractServiceEntry
     {
-        private readonly object? FInstance;
-
-        private readonly Func<IServiceRegistry, object> FSelector;
+        private object? FInstance;
 
         private ContextualServiceEntry(ContextualServiceEntry original, IServiceRegistry owner) : base(original.Interface, original.Name, null, Ensure.Parameter.IsNotNull(owner, nameof(owner)))
         {
-            FSelector = original.FSelector;
-            FInstance = FSelector(owner);
+            Selector = original.Selector;
             State = ServiceEntryStates.Built;
         }
 
-        public ContextualServiceEntry(Type @interface, string? name, Func<IServiceRegistry, object> selector) : base(@interface, name, null, null)
-        {
-            FSelector = Ensure.Parameter.IsNotNull(selector, nameof(selector));
-        }
+        public ContextualServiceEntry(Type @interface, string? name, Func<IServiceRegistry, object> selector) : base(@interface, name, null, null) =>
+            Selector = selector;
 
         public override object CreateInstance(IInjector scope) => throw new InvalidOperationException();
 
-        public override object GetSingleInstance() => FInstance ?? throw new InvalidOperationException();
+        public override object GetSingleInstance()
+        {
+            if (Owner is null)
+                throw new InvalidOperationException();
+
+            return FInstance ??= Selector(Owner);
+        }
+
+        public Func<IServiceRegistry, object> Selector { get; }
 
         public override AbstractServiceEntry CopyTo(IServiceRegistry owner) => new ContextualServiceEntry(this, Ensure.Parameter.IsNotNull(owner, nameof(owner)));
     }
