b3d03dae41ebfe97115136d584437e2939428fc6
Dénes Solti <sodnaatx@gmail.com>
2021-10-01 16:12:35 +0200

    ServiceRegistry.cs: use DictionarySlim to store generic entries

--- SRC/Injector/Private/ServiceRegistry/ServiceRegistry.cs
+++ SRC/Injector/Private/ServiceRegistry/ServiceRegistry.cs
@@ -5,9 +5,10 @@
 ********************************************************************************/
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Threading;
+
+using Microsoft.Collections.Extensions;
 
 namespace Solti.Utils.DI.Internals
 {
@@ -27,40 +28,43 @@ namespace Solti.Utils.DI.Internals
         private readonly AbstractServiceEntry?[] FRegularEntries;
 
         //
+        // DictionarySlim-nek olyan kulcs kell ami megvalositja az IEquatable interface-t
+        //
+
+        private sealed record TypeWrap(Type Type);
+
+        //
         // Ez NE egyetlen szotar legyen mert ott a neveket is szamon kene tartsuk amivel viszont
         // jelentosen lassulna a bejegyzes lekerdezes.
         //
 
-        private readonly HybridDictionary?[] FSpecializedEntries;
+        private readonly DictionarySlim<TypeWrap, AbstractServiceEntry>?[] FSpecializedEntries;
 
         public ServiceRegistry(ISet<AbstractServiceEntry> entries, ResolverBuilder? resolverBuilder = null, CancellationToken cancellation = default) : base(entries, resolverBuilder, cancellation)
         {
             FRegularEntries = new AbstractServiceEntry?[RegularEntryCount];
-            FSpecializedEntries = new HybridDictionary?[GenericEntryCount];
+            FSpecializedEntries = new DictionarySlim<TypeWrap, AbstractServiceEntry>?[GenericEntryCount];
         }
 
         public ServiceRegistry(ServiceRegistryBase parent) : base(parent)
         {
             FRegularEntries = new AbstractServiceEntry?[RegularEntryCount];
-            FSpecializedEntries = new HybridDictionary?[GenericEntryCount];
+            FSpecializedEntries = new DictionarySlim<TypeWrap, AbstractServiceEntry>?[GenericEntryCount];
         }
 
         public override AbstractServiceEntry ResolveGenericEntry(int slot, Type specializedInterface, AbstractServiceEntry originalEntry)
         {
             Debug.Assert(specializedInterface.IsConstructedGenericType, $"{nameof(specializedInterface)} must be a closed generic type");
 
-            ref HybridDictionary? specializedEntries = ref FSpecializedEntries[slot];
+            ref DictionarySlim<TypeWrap, AbstractServiceEntry>? specializedEntries = ref FSpecializedEntries[slot];
             if (specializedEntries is null)
-                specializedEntries = new HybridDictionary();
+                specializedEntries = new DictionarySlim<TypeWrap, AbstractServiceEntry>();
 
-            AbstractServiceEntry? specializedEntry = (AbstractServiceEntry?) specializedEntries[specializedInterface];
-
+            ref AbstractServiceEntry specializedEntry = ref specializedEntries.GetOrAddValueRef(new TypeWrap(specializedInterface));
             if (specializedEntry is null)
             {
                 ISupportsSpecialization supportsSpecialization = (ISupportsSpecialization) originalEntry;
-
                 specializedEntry = supportsSpecialization.Specialize(this, specializedInterface.GenericTypeArguments);
-                specializedEntries[specializedInterface] = specializedEntry;
             }
 
             return specializedEntry;
