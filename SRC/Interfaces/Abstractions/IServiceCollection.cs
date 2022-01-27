4dc411d771ee7d5c1257ddb335c23fe6417fef83
Dénes Solti <sodnaatx@gmail.com>
2022-01-27 06:56:32 +0100

    expose ServiceIdComparer class

--- SRC/Interfaces/Abstractions/IServiceCollection.cs
+++ SRC/Interfaces/Abstractions/IServiceCollection.cs
@@ -10,7 +10,14 @@ namespace Solti.Utils.DI.Interfaces
     /// <summary>
     /// Specifies the contract for an <see cref="AbstractServiceEntry"/> set.
     /// </summary>
+    /// <remarks><see cref="IServiceCollection"/> interface implements the <see cref="ISet{AbstractServiceEntry}"/> interface so you can register only one entry with the given <see cref="AbstractServiceEntry.Interface"/> and <see cref="AbstractServiceEntry.Name"/>.</remarks>
     public interface IServiceCollection : ISet<AbstractServiceEntry>
     {
+#if NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Comparer to be used to determine service entry equality.
+        /// </summary>
+        public static IEqualityComparer<IServiceId> Comparer { get; } = ServiceIdComparer.Instance;
+#endif
     }
 }
--- SRC/Interfaces/ServiceIdComparer.cs
+++ SRC/Interfaces/ServiceIdComparer.cs
@@ -5,20 +5,24 @@
 ********************************************************************************/
 using System;
 
-namespace Solti.Utils.DI.Internals
+namespace Solti.Utils.DI.Interfaces
 {
-    using Interfaces;
     using Primitives;
 
-    internal sealed class ServiceIdComparer : ComparerBase<ServiceIdComparer, IServiceId>
+    /// <summary>
+    /// Specifies how to compare <see cref="IServiceId"/> instances.
+    /// </summary>
+    public sealed class ServiceIdComparer : ComparerBase<ServiceIdComparer, IServiceId>
     {
-        public override bool Equals(IServiceId x, IServiceId y) => x.Interface == y.Interface && x.Name == y.Name;
+        /// <inheritdoc/>
+        public override bool Equals(IServiceId x, IServiceId y) => x?.Interface == y?.Interface && x?.Name == y?.Name;
 
+        /// <inheritdoc/>
         public override int GetHashCode(IServiceId obj) =>
 #if NETSTANDARD2_1_OR_GREATER
-            HashCode.Combine(obj.Interface, obj.Name)
+            HashCode.Combine(obj?.Interface, obj?.Name)
 #else
-            new { obj.Interface, obj.Name }.GetHashCode()        
+            new { obj?.Interface, obj?.Name }.GetHashCode()        
 #endif
             ;
     }
