bd1621f45ebe0416fa9f54c79586d7b9356f0ec1
Dénes Solti <sodnaatx@gmail.com>
2022-04-23 10:20:05 +0200

    fix misleading test fixture name

--- TEST/Injector.Tests/VariableCapturing.cs
+++ TEST/Injector.Tests/VariableCapturing.cs
@@ -1,5 +1,5 @@
 ﻿/********************************************************************************
-* ExperimantelScope.cs                                                          *
+* VariableCapturing.cs                                                          *
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
@@ -10,7 +10,7 @@ using NUnit.Framework;
 namespace Solti.Utils.DI.Internals.Tests
 {
     [TestFixture]
-    public class ExperimantelScopeTests
+    public class VariableCapturingTests
     {
         [Test]
         public void VariableCapturingTest()
