07ebd75a0a4e2b9e43ad728aef4730fa7995899e
Dénes Solti <sodnaatx@gmail.com>
2022-05-22 09:01:54 +0200

    test expression chain performance

---
+++ PERF/ExpressionChain.cs
@@ -0,0 +1,35 @@
+﻿/********************************************************************************
+* ExpressionChain.cs                                                            *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq.Expressions;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.DI.Perf
+{
+    [Ignore]
+    [MemoryDiagnoser]
+    public class ExpressionChain
+    {
+        private readonly Func<int, int> Fn;
+
+        public ExpressionChain()
+        {
+            Expression<Func<int, int>> expr = i => i + 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                ParameterExpression p = Expression.Parameter(typeof(int));
+                expr = Expression.Lambda<Func<int, int>>(Expression.Invoke(expr, Expression.Add(p, Expression.Constant(1))), p);
+            }
+
+            Fn = expr.Compile();
+        }
+
+        [Benchmark]
+        public void Invoke() => Fn(0);
+    }
+}
