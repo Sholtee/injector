aafe3562957a68c61e7b1deffe46857a2c662ea2
Dénes Solti <sodnaatx@gmail.com>
2022-05-22 16:38:36 +0200

    ScopeFactory.cs: fix typo

--- SRC/Injector/Public/ScopeFactory.cs
+++ SRC/Injector/Public/ScopeFactory.cs
@@ -20,15 +20,15 @@ namespace Solti.Utils.DI
         /// </summary>
         /// <param name="registerServices">The callback to register services.</param>
         /// <param name="options">The <see cref="ScopeOptions"/> to be applied against all the created scopes.</param>
-        /// <param name="lifetimne">Optional hint specifying the object that is responsible for releasing the root scope</param>
-        public static IScopeFactory Create(Action<IServiceCollection> registerServices, ScopeOptions? options = null, object? lifetimne = null)
+        /// <param name="lifetime">Optional hint specifying the object that is responsible for releasing the root scope</param>
+        public static IScopeFactory Create(Action<IServiceCollection> registerServices, ScopeOptions? options = null, object? lifetime = null)
         {
             Ensure.Parameter.IsNotNull(registerServices, nameof(registerServices));
 
             ServiceCollection services = new();
             registerServices(services);
 
-            return Create(services, options, lifetimne);
+            return Create(services, options, lifetime);
         }
 
         /// <summary>
@@ -36,16 +36,16 @@ namespace Solti.Utils.DI
         /// </summary>
         /// <param name="services">Register services.</param>
         /// <param name="options">The <see cref="ScopeOptions"/> to be applied against all the created scopes.</param>
-        /// <param name="lifetimne">Optional hint specifying the object that is responsible for releasing the root scope</param>
-        public static IScopeFactory Create(IServiceCollection services, ScopeOptions? options = null, object? lifetimne = null)
+        /// <param name="lifetime">Optional hint specifying the object that is responsible for releasing the root scope</param>
+        public static IScopeFactory Create(IServiceCollection services, ScopeOptions? options = null, object? lifetime = null)
         {
             Ensure.Parameter.IsNotNull(services, nameof(services));
 
             options ??= new();
 
             return options.SupportsServiceProvider
-                ? new InjectorSupportsServiceProvider(services, options, lifetimne)
-                : new Injector(services, options, lifetimne);
+                ? new InjectorSupportsServiceProvider(services, options, lifetime)
+                : new Injector(services, options, lifetime);
         }
     }
 }
