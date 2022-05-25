582dee39c51bde67bbd1fc2da51a347fd5fc38ba
Dénes Solti <sodnaatx@gmail.com>
2022-05-25 09:00:02 +0200

    introduce ServiceRequestVisitor class

---
+++ SRC/Injector/Private/ServiceRequestVisitor.cs
@@ -0,0 +1,64 @@
+﻿/********************************************************************************
+* ServiceRequestVisitor.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Solti.Utils.DI.Internals
+{
+    using Interfaces;
+
+    internal sealed class ServiceRequestVisitor : ExpressionVisitor
+    {
+        private static readonly MethodInfo[]
+            FInjectorGet = new[]
+            {
+                MethodInfoExtractor.Extract<IInjector>(i => i.Get(null!, null)),
+                MethodInfoExtractor.Extract<IInjector>(i => i.TryGet(null!, null))
+            },
+            FGenericInjectorGet = new[] {
+                MethodInfoExtractor.Extract<IInjector>(i => i.Get<object>(null)).GetGenericMethodDefinition(),
+                MethodInfoExtractor.Extract<IInjector>(i => i.TryGet<object>(null)).GetGenericMethodDefinition()
+            };
+
+        public event Func<MethodCallExpression, Expression, Type, string?, Expression>? VisitServiceRequest;
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (VisitServiceRequest is not null)
+            {
+                if (!node.Method.IsGenericMethod)
+                {
+                    if (Array.IndexOf(FInjectorGet, node.Method) >= 0 && node.Arguments[0] is ConstantExpression iface && node.Arguments[1] is ConstantExpression name)
+                    {
+                        return VisitServiceRequest
+                        (
+                            node,
+                            node.Object,
+                            (Type)iface.Value,
+                            (string?)name.Value
+                        );
+                    }
+                }
+                else
+                {
+                    if (Array.IndexOf(FGenericInjectorGet, node.Method.GetGenericMethodDefinition()) >= 0 && node.Arguments[1] is ConstantExpression name)
+                    {
+                        return VisitServiceRequest
+                        (
+                            node,
+                            node.Arguments[0],
+                            node.Method.GetGenericArguments()[0],
+                            (string?) name.Value
+                        );
+                    }
+                }
+            }
+
+            return base.VisitMethodCall(node);
+        }
+    }
+}
---
+++ TEST/Injector.Tests/ServiceRequestVisitor.cs
@@ -0,0 +1,92 @@
+﻿/********************************************************************************
+* ServiceRequestVisitor.cs                                                      *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.DI.Internals.Tests
+{
+    using Interfaces;
+
+    [TestFixture]
+    public class ServiceRequestVisitorTests
+    {
+        public static IEnumerable<Expression<Action<IInjector>>> Expressions
+        {
+            get
+            {
+                yield return i => i.Get(typeof(IList), "cica");
+                yield return i => i.TryGet(typeof(IList), "cica");
+                yield return i => i.Get<IList>("cica");
+                yield return i => i.TryGet<IList>("cica");
+            }
+        }
+
+        [Test]
+        public void Visit_ShouldVisitAllTheInjectorInvocations([ValueSource(nameof(Expressions))] Expression<Action<IInjector>> expresision)
+        {
+            List<(Type Interface, string Name)> requests = new(); 
+
+            ServiceRequestVisitor visitor = new();
+            visitor.VisitServiceRequest += (orig, _, iface, name) =>
+            {
+                requests.Add((iface, name));
+                return orig;
+            };
+            visitor.Visit(expresision);
+
+            Assert.That(requests.Count, Is.EqualTo(1));
+            Assert.That(requests[0].Interface, Is.EqualTo(typeof(IList)));
+            Assert.That(requests[0].Name, Is.EqualTo("cica"));
+        }
+
+        public static IEnumerable<Expression<Action<IInjector>>> ElaborateExpressions
+        {
+            get
+            {
+                yield return i => i.Get(typeof(IList), new string("cica"));
+                yield return i => i.Get<IList>(new string("cica"));
+            }
+        }
+
+        [Test]
+        public void Visit_ShouldSkipInjectorInvocationsHavingNonConstantParameter([ValueSource(nameof(ElaborateExpressions))] Expression<Action<IInjector>> expresision)
+        {
+            List<(Type Interface, string Name)> requests = new();
+
+            ServiceRequestVisitor visitor = new();
+            visitor.VisitServiceRequest += (orig, _, iface, name) =>
+            {
+                requests.Add((iface, name));
+                return orig;
+            };
+            visitor.Visit(expresision);
+
+            Assert.That(requests.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Visit_ShouldSupportNull()
+        {
+            List<(Type Interface, string Name)> requests = new();
+
+            ServiceRequestVisitor visitor = new();
+            visitor.VisitServiceRequest += (orig, _, iface, name) =>
+            {
+                requests.Add((iface, name));
+                return orig;
+            };
+            visitor.Visit((Expression<Action<IInjector>>) (i => i.Get(null, null)));
+
+            Assert.That(requests.Count, Is.EqualTo(1));
+            Assert.That(requests[0].Interface, Is.Null);
+            Assert.That(requests[0].Name, Is.Null);
+        }
+    }
+}
