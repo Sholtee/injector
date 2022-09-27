71fa22bef2cf7bd750429dd009b281c0cfede7dc
Dénes Solti <sodnaatx@gmail.com>
2022-09-27 15:04:36 +0200

    introduce IDelegateCompiler interface

---
+++ SRC/Interfaces/Abstractions/IDelegateCompiler.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* IDelegateCompiler.cs                                                          *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq.Expressions;
+
+namespace Solti.Utils.DI.Interfaces
+{
+    /// <summary>
+    /// Specifies the contract of delegate compilers.
+    /// </summary>
+    public interface IDelegateCompiler
+    {
+        /// <summary>
+        /// Compiles the given <paramref name="expression"/> in a deferred manner.
+        /// </summary>
+        void Compile<TDelegate>(Expression<TDelegate> expression, Action<TDelegate> completionCallback);
+
+        /// <summary>
+        /// Compiles all the delegates filed by the <see cref="Compile{TDelegate}(Expression{TDelegate}, Action{TDelegate})"/> method.
+        /// </summary>
+        void Compile();
+    }
+}
