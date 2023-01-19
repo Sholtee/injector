0ca6fc7ea586a4c25231218f6480c463d93af49a
Dénes Solti <sodnaatx@gmail.com>
2023-01-19 07:50:39 +0100

    add missing type forwards

---
+++ SRC/Injector/Public/ScopeOptions.cs
@@ -0,0 +1,11 @@
+﻿/********************************************************************************
+* ScopeOptions.cs                                                               *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+#pragma warning disable RS0016  // workaround for bug in Microsoft.CodeAnalysis.PublicApiAnalyzers when trying to forward record types
+using System.Runtime.CompilerServices;
+
+using Solti.Utils.DI.Interfaces;
+
+[assembly: TypeForwardedTo(typeof(ScopeOptions))]
