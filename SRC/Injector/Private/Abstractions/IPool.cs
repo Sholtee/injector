13c1eb7539cbecc83dc4b30dd3b1fe7c8ad81e49
Dénes Solti <sodnaatx@gmail.com>
2023-01-20 06:27:45 +0100

    descend IPool from IDisposable

--- SRC/Injector/Private/Abstractions/IPool.cs
+++ SRC/Injector/Private/Abstractions/IPool.cs
@@ -3,9 +3,11 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
+using System;
+
 namespace Solti.Utils.DI.Internals
 {
-    internal interface IPool
+    internal interface IPool: IDisposable
     {
         object Get();
 
