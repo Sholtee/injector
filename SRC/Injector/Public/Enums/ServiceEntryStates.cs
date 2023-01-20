338dc10adb6c0629a48e619e9112acceb73dd987
Dénes Solti <sodnaatx@gmail.com>
2023-01-20 08:11:31 +0100

    add some type forwards

--- SRC/Injector/Public/Enums/ServiceEntryStates.cs
+++ SRC/Injector/Public/Enums/ServiceEntryStates.cs
@@ -1,10 +1,9 @@
 ﻿/********************************************************************************
-* ServiceOptions.cs                                                             *
+* ServiceEntryStates.cs                                                         *
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System.Runtime.CompilerServices;
 using Solti.Utils.DI.Interfaces;
 
-#pragma warning disable RS0016  // workaround for bug in Microsoft.CodeAnalysis.PublicApiAnalyzers when trying to forward record types
-[assembly: TypeForwardedTo(typeof(ServiceOptions))]
+[assembly: TypeForwardedTo(typeof(ServiceEntryStates))]
