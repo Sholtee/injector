7f1a9f72c9c83255363bf1771b56a7570f71445a
Dénes Solti <sodnaatx@gmail.com>
2023-05-22 11:09:14 +0200

    drop unused files

--- SRC/Injector/Public/Abstractions/IInterfaceInterceptor2.cs
+++
@@ -1,9 +0,0 @@
-/********************************************************************************
-* IInterfaceInterceptor.cs                                                      *
-*                                                                               *
-* Author: Denes Solti                                                           *
-********************************************************************************/
-using Solti.Utils.DI.Interfaces;
-using System.Runtime.CompilerServices;
-
-[assembly: TypeForwardedTo(typeof(IInterfaceInterceptor2))]
