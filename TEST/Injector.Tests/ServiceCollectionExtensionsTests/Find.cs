d778a5403e27da0f4838348bf9d05064d97c9b3f
denes.solti <sodnaatx@gmail.com>
2023-12-27 10:54:50 +0100

    increase coverage V

--- TEST/Injector.Tests/ServiceCollectionExtensionsTests/Find.cs
+++ TEST/Injector.Tests/ServiceCollectionExtensionsTests/Find.cs
@@ -24,10 +24,26 @@ namespace Solti.Utils.DI.Tests
         }
 
         [Test]
+        public void TryFind_ShouldBeNullChecked()
+        {
+            Assert.Throws<ArgumentNullException>(() => IServiceCollectionBasicExtensions.TryFind<IInterface_1>(null));
+            Assert.Throws<ArgumentNullException>(() => IServiceCollectionBasicExtensions.TryFind<IInterface_1>(null, null));
+            Assert.Throws<ArgumentNullException>(() => Collection.TryFind(null));
+            Assert.Throws<ArgumentNullException>(() => Collection.TryFind(null, null));
+        }
+
+        [Test]
         public void Find_ShouldThrowOnMissingService()
         {
             Assert.Throws<ServiceNotFoundException>(() => Collection.Find<IInterface_1>());
             Assert.Throws<ServiceNotFoundException>(() => Collection.Find<IInterface_1>(null));
+        }
+
+        [Test]
+        public void TryFind_ShouldReturnNullOnMissingService()
+        {
+            Assert.That(Collection.TryFind<IInterface_1>(), Is.Null);
+            Assert.That(Collection.TryFind<IInterface_1>(null), Is.Null);
         }
 
         [Test]
@@ -36,6 +52,18 @@ namespace Solti.Utils.DI.Tests
             Collection.Service<IInterface_1, Implementation_1_No_Dep>(name, Lifetime.Transient);
 
             AbstractServiceEntry svc = Collection.Find<IInterface_1>(name);
+
+            Assert.That(svc, Is.Not.Null);
+            Assert.That(svc.Type, Is.EqualTo(typeof(IInterface_1)));
+            Assert.That(svc.Key, Is.EqualTo(name));
+        }
+
+        [Test]
+        public void TryFind_ShouldReturnTheProperService([Values(null, "name")] string name)
+        {
+            Collection.Service<IInterface_1, Implementation_1_No_Dep>(name, Lifetime.Transient);
+
+            AbstractServiceEntry svc = Collection.TryFind<IInterface_1>(name);
 
             Assert.That(svc, Is.Not.Null);
             Assert.That(svc.Type, Is.EqualTo(typeof(IInterface_1)));
